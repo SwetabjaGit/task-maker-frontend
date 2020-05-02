6facdbf722ef4def862088c0dccfcab5ba88a757
Swetabja Hazra <swetabjahazra@Swetabjas-Awesome-Mac.local>
2020-05-02 11:13:23 +0530

    Whitepage fix 8

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -65,7 +65,7 @@ module.exports = function(webpackEnv) {
   // It requires a trailing slash, or the file assets will get an incorrect path.
   // In development, we always serve from the root. This makes config easier.
   const publicPath = isEnvProduction
-    ? paths.servedPath
+    ? paths.servedPath + '/build/'
     : isEnvDevelopment && '/';
   // Some apps do not use client-side routing with pushState.
   // For these, "homepage" can be set to "." to enable relative asset paths.
